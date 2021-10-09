048769721ef3b8cd608f10d496d982531ecf043c
jawsnnn <arpanmalviya@gmail.com>
2021-10-09 22:35:48 +0000

    new code

--- src/main.py
+++ src/main.py
@@ -1,13 +1,32 @@
 '''
 Application file
 '''
+from typing import Optional
 
+from pydantic import BaseModel
 from fastapi import FastAPI
 
-application = FastAPI()
+app = FastAPI()
 
-@application.get("/")
-def get_root(message):
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
+@app.get("/users/me")
+async def get_user_me():
     return {
-        "message": f"This is a fresh message: {message}"
+        "user" : "myself"
     }
+
+@app.get("/users/{username}")
+async def get_user(username: str):
+    return {
+        "user": username
+    }
+
+@app.post("/items/")
+def create_item(item: Item):
+    return item
